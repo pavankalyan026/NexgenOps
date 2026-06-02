9dd9f2aff355e4e52c9a41413e552ded914be966
Pavankalyan koneti <124763316+pavankalyan026@users.noreply.github.com>
2026-06-02 19:06:32 +0530

    Change database URL to MySQL connection

--- database/db.py
+++ database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///nexgenops.db"
+DATABASE_URL=mysql+pymysql://user:password@host/dbname
 
 engine = create_engine(
     DATABASE_URL,
